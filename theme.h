8f214a493c0b779bfc3405a72caf43734b334807
z* <79771590+dg4l@users.noreply.github.com>
2025-04-05 15:16:56 -0400

    Delete wrongly placed theme.h

--- theme.h
+++
@@ -1,6 +0,0 @@
-static const char col_gray1[]       = "#1a1b26";
-static const char col_gray2[]       = "#ff9e64";
-static const char col_gray3[]       = "#f7768e";
-static const char col_gray4[]       = "#7aa2f7";
-static const char col_cyan[]        = "#24283b";
-static const char col_border[]      = "#bb9af7";
