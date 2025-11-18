c0069ea1df65899eae600fdf995cedd70222e8b0
dg4l <79771590+dg4l@users.noreply.github.com>
2025-11-18 08:45:45 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "please run as root." 
+   exit 1
+fi
+
+components=("dwm" "slstatus" "dmenu")
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd "$SCRIPTPATH"
+cp -r ./.wm-scripts/ "$USER_HOME" 
+for component in "${components[@]}"; do
+    cd "$component"
+    make clean install
+    cd $SCRIPTPATH
+done
