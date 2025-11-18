ca69c1ac0a21587aa8d4b36bdecfc9516d537e89
dg4l <79771590+dg4l@users.noreply.github.com>
2025-11-18 09:14:07 -0500

    ..

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 components=("dwm" "slstatus" "dmenu")
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 cd "$SCRIPTPATH"
 cp -r ./.wm-scripts/ "$USER_HOME"
 for component in "${components[@]}"; do
