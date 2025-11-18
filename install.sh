a2c0b85ed61a8cafa9ba57c386b7fb62c8af49f7
dg4l <79771590+dg4l@users.noreply.github.com>
2025-11-18 08:46:34 -0500

    add disclaimer and remove trailing spaces

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+# RUN AT YOUR OWN RISK
 if [[ $EUID -ne 0 ]]; then
-   echo "please run as root." 
+   echo "please run as root."
    exit 1
 fi
-
-components=("dwm" "slstatus" "dmenu")
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+components=("dwm" "slstatus" "dmenu")
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "$SCRIPTPATH"
-cp -r ./.wm-scripts/ "$USER_HOME" 
+cp -r ./.wm-scripts/ "$USER_HOME"
 for component in "${components[@]}"; do
     cd "$component"
     make clean install
